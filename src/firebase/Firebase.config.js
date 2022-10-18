b04e9018abedb0483ee8a7946fab4c8e5e9d6640
Shariful Islam <engr.shariful.cse@gmail.com>
2022-10-19 01:59:43 +0200

    Created news portal site

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF4tGqfPF3_Zz-6R0b5RH_PzLhy8rfEVY",
+  authDomain: "dragon-news-32bbe.firebaseapp.com",
+  projectId: "dragon-news-32bbe",
+  storageBucket: "dragon-news-32bbe.appspot.com",
+  messagingSenderId: "148179328107",
+  appId: "1:148179328107:web:c9fa117c0cb4594b63252e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
