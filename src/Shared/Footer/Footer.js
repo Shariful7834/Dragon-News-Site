ebf2db563b756ba03dba15474da94e1e1210304f
Shariful Islam <engr.shariful.cse@gmail.com>
2022-10-19 02:41:01 +0200

    implemented the structured and installed the basic npm settings

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>Footer Page</h2>
+      <p>CopyRight COming soon...</p>
+    </div>
+  );
+};
+
+export default Footer;
